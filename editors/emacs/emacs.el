0a99ea9cefe09f20546cf5dad083bf598e26b28b
Varun Agrawal <varagrawal@gmail.com>
2023-05-22 16:57:34 -0400

    fix emacs theme loading

--- editors/emacs/emacs.el
+++ editors/emacs/emacs.el
@@ -30,26 +30,8 @@
   '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(custom-enabled-themes (quote (foggy-night)))
- '(custom-safe-themes
-   (quote
-    ("f6cdb429a64db06d3db965871b45ed1c666fdce2d3e2c4b810868e4cf4244c92" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "246cd0eb818bfd347b20fb6365c228fddf24ab7164752afe5e6878cb29b0204e" "2593436c53c59d650c8e3b5337a45f0e1542b1ba46ce8956861316e860b145a0" default)))
- '(package-selected-packages (quote (spacemacs-theme foggy-night-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+; Load theme
+(load-theme 'dakrone t)
 
 ; set different themes for window and terminal versions
 (if (display-graphic-p) 
