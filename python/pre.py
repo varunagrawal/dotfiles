2e37bbbebbe3176f1b44ec44a1a9378a7b824be9
Varun Agrawal <varagrawal@gmail.com>
2025-11-22 13:35:50 -0500

    add jupyter imports within try-except block

--- python/pre.py
+++ python/pre.py
@@ -1,8 +1,21 @@
-import gtdynamics as gtd
-# Robotics
-import gtsam
+"""Modules imported by default when starting a new IPython kernel."""
+
 import numpy as np
 import scipy as scp
-# Machine Learning
-import torch
-import torchvision
+
+# Robotics
+try:
+    import gtsam
+    import gtdynamics as gtd  # isort: skip
+except ImportError as exc:
+    print("gtsam (or sister library) not imported.")
+    print(exc.msg)
+
+
+# # Machine Learning
+# try:
+#     import torch
+#     import torchvision
+# except ImportError as exc:
+#     print("A machine learning library was not imported.")
+#     print(exc.msg)
